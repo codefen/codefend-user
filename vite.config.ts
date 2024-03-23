4717c7571feae0da1145309f4d47dbb18d260106
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-03-23 00:16:30 -0300

    config lightning css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import LightningCSS from 'unplugin-lightningcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-	plugins: [react()],
-
+export default defineConfig( () => ({
+	build: {
+		cssMinify: "lightningcss" as "lightningcss",
+	},
+	plugins: [react(), LightningCSS()],
 	clearScreen: false,
 	server: {
 		port: 3000,
