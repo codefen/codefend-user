6f13ad7129657c73bda8bd7721c393a8a58e6338
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-04-11 21:07:00 -0300

    update mobile fallback

---
+++ src/app/views/components/defaults/mobile-fallback/MobileFallback.tsx
@@ -0,0 +1,20 @@
+import './mobilefallback.scss';
+
+export const MobileFallback = () => (
+	<div className="fallback-container">
+		<picture className="logo-container">
+			<img src="/codefend/fav.png" alt="Codefend logo" />
+		</picture>
+
+		<div className="fallback-content">
+			<h1 className="title">Not available</h1>
+			<div className="text-container">
+				<p>We continue working on the version for mobile devices</p>
+				<p>
+					We invite you to continue working from a device with higher
+					resolution
+				</p>
+			</div>
+		</div>
+	</div>
+);
