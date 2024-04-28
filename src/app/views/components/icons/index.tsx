95f40e63fc36c36951f3891c6503366bbc0f1217
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-04-28 02:48:26 -0300

    update reseller icons

--- src/app/views/components/icons/index.tsx
+++ src/app/views/components/icons/index.tsx
@@ -1318,3 +1318,40 @@ export const CredentialIcon: FC<IconProps> = () => (
 		<path d="m0 0h32v32h-32z" fill="none" />
 	</svg>
 );
+
+export const LeadIcon: FC<IconProps> = ({ isVisible }) => (
+	<svg
+		height="1.25rem"
+		width="1.25rem"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1.5}
+		stroke="currentColor"
+		style={{
+			overflow: isVisible ? 'visible' : 'auto',
+		}}>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+		/>
+	</svg>
+);
+export const UsersIcon: FC<IconProps> = ({ isVisible }) => (
+	<svg
+		height="1.25rem"
+		width="1.25rem"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1.5}
+		stroke="currentColor"
+		style={{
+			overflow: isVisible ? 'visible' : 'auto',
+		}}>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+		/>
+	</svg>
+);
