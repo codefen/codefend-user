317c050dafaadf5a2dfdb9a56c864b3bdcfd6eef
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-12-12 05:04:57 -0300

    fix: update rust dependencies and fixe tauri prod wkf

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,64 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate cocoa;
+
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
+
+use tauri::Emitter;
+use tauri::{WebviewUrl, WebviewWindowBuilder};
+use tauri_plugin_dialog;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init());
+
+    #[cfg(target_os = "macos")]
+    let builder = builder.plugin(tauri_traffic_light_positioner_plugin::init());
+
+    builder
+        .setup(|#[allow(unused_variables)] app| {
+            #[cfg(desktop)]
+            {
+                #[cfg(desktop)]
+                if cfg!(debug_assertions) {
+                    app.handle().plugin(
+                        tauri_plugin_log::Builder::default()
+                            .level(log::LevelFilter::Info)
+                            .build(),
+                    )?;
+                }
+                let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                    .title("")
+                    .inner_size(800.0, 600.0)
+                    .resizable(true)
+                    .maximized(true);
+
+                #[cfg(target_os = "macos")]
+                let win_builder = win_builder
+                    .decorations(true)
+                    .title_bar_style(TitleBarStyle::Overlay);
+
+                #[cfg(not(target_os = "macos"))]
+                let win_builder = win_builder.decorations(false).transparent(true);
+
+                win_builder.build().unwrap();
+
+                app.handle().emit("window-ready", {}).unwrap();
+
+                Ok(())
+            }
+        })
+        .build(tauri::generate_context!())
         .expect("error while running tauri application");
 }
