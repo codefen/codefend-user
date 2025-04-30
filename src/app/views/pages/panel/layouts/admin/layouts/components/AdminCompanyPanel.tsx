d47016d6a8aad8c2c5bc685087c7a537786922db
Vanina <vaniibalbastro@gmail.com>
2025-04-30 10:23:00 -0300

    feat(company-panel): improve search bar and table layout

--- src/app/views/pages/panel/layouts/admin/layouts/components/AdminCompanyPanel.tsx
+++ src/app/views/pages/panel/layouts/admin/layouts/components/AdminCompanyPanel.tsx
@@ -5,9 +5,9 @@ export const AdminCompanyPanel: FC = () => {
   return (
     <>
       <div className="company-header-bar internal-tables">
-        <div className="header-bar-options internal-tables-active">
+        {/* <div className="header-bar-options internal-tables-active">
           <p className="text-small title-format current-company">Current companies</p>
-        </div>
+        </div> */}
       </div>
 
       <CompanyIndexView />
