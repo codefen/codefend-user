e6dda6b4cd49f1cf88b9c4ceb25f86fffa2b6718
chris <chris.russo99@gmail.com>
2025-05-20 18:08:05 -0300

    español a ingles

--- src/app/views/components/DashboardScanStart/DashboardScanStart.tsx
+++ src/app/views/components/DashboardScanStart/DashboardScanStart.tsx
@@ -65,7 +65,7 @@ export const DashboardScanStart = () => {
                 <StatAsset value={currentScan.get?.issues_found} valueTitle="Total Findings" />
                 <StatAsset
                   value={`${currentScan.get?.issues_found}/${currentScan.get?.issues_parsed}`}
-                  valueTitle="Analisis completado"
+                  valueTitle="Analyzed findings"
                 />
                 <StatAsset value={`${Math.round(scanProgress.get)}%`} valueTitle="Progress" />
               </div>
