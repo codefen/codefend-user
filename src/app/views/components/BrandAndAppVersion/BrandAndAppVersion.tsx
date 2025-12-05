24d827e3871019ffb19132e42c4c1648403a4e0e
MarcossIC <marcoslopezdev18@gmail.com>
2025-12-05 12:37:47 -0300

    Update version

--- src/app/views/components/BrandAndAppVersion/BrandAndAppVersion.tsx
+++ src/app/views/components/BrandAndAppVersion/BrandAndAppVersion.tsx
@@ -4,7 +4,7 @@ const Logo = lazy(() => import('../Logo/Logo'));
 
 const BrandAndAppVersion: FC = () => (
   <div className="brand">
-    <span>v25.1.0</span>
+    <span>v25.1.1</span>
     <Logo theme={'shadow'} />
   </div>
 );
